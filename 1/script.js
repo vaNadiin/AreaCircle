3ca4d452778b7c8bc2d453c3e538e61218c97f8c
vaNadiin <glivpiv@gmail.com>
2023-11-19 12:46:57 +0300

    add calculation circumference

--- 1/script.js
+++ 1/script.js
@@ -9,7 +9,13 @@ class Circle {
 		let areaCircle = Math.PI * Math.pow(this.#radius, 2);
 		return areaCircle;
 	}
+
+	getСircumference() {
+		let circumference = 2 * Math.PI * this.#radius;
+		return circumference;
+	}
 }
 
 let circle = new Circle('3');
 console.log(circle.getAreaCircle());
+console.log(circle.getСircumference());
